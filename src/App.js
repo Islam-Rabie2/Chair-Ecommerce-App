9c827c5171f9ed67304f796f80294d3cb483f592
Islam-Rabie2 <islam.rabie25@gmail.com>
2024-06-13 14:02:06 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from "./common/Header";
+import Footer from "./common/Footer";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFoundPage from "./pages/NotFoundPage";
+import Shop from "./pages/Shop";
+import ContactUs from "./pages/Contact";
+import AboutUs from "./pages/AboutUs";
+import Blog from "./pages/Blog";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+    {/* for suggestion we need to hit ctrl + space */}
+  </div>
+
   );
 }
 
